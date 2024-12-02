c6b3ee4a3d4fc118dc69527e078bfbdb61ae072a
Jayden Martinez <145072355+SlashNDash96@users.noreply.github.com>
2024-12-02 11:13:56 -0800

    15 and 16
    Did Challenge 15/16

--- CodingChallengesMartinezP3/Program.cs
+++ CodingChallengesMartinezP3/Program.cs
@@ -4,7 +4,6 @@ class Challenges
 {
     public static void Main(string[] args)
     {
-        // Existing code (unchanged)
         Console.WriteLine("Welcome to my Coding Challenge program! There will be a variety of different functions you will \nhave to choose from. Please pick one by entering the corresponding number.");
         Console.WriteLine("1: Sum - Add two numbers");
         Console.WriteLine("2: Convert minutes to seconds");
@@ -21,6 +20,7 @@ class Challenges
         Console.WriteLine("13: Return the sum of a polygon");
         Console.WriteLine("14: Return a phrase with Edabit");
         Console.WriteLine("15: Logical AND of two booleans");
+        Console.WriteLine("16: Calculate and Return basketball points");
         Console.WriteLine("0: Exit");
 
         while (true)
@@ -80,176 +80,196 @@ class Challenges
                 case "15":
                     LogicalAndExample();
                     break;
+                case "16":
+                    CalculateBasketballPoints();
+                    break;
                 default:
                     Console.WriteLine("Invalid option, please choose a valid function.");
                     break;
             }
         }
-    }
+   
+            static void CalculateBasketballPoints()
+            {
+                Console.WriteLine("Please enter the number of 2-pointers scored:");
+                int twoPointers = int.Parse(Console.ReadLine());
 
-    static void LogicalAndExample()
-    {
-        Console.WriteLine("Testing Logical AND (&&) with true and false:");
+                Console.WriteLine("Please enter the number of 3-pointers scored:");
+                int threePointers = int.Parse(Console.ReadLine());
 
-     
-        Console.WriteLine("Enter the first boolean value (true/false):");
-        bool firstInput = ConvertToBool(Console.ReadLine());
+                Console.WriteLine($"The total points scored is: {Points(twoPointers, threePointers)}");
+            }
 
-        Console.WriteLine("Enter the second boolean value (true/false):");
-        bool secondInput = ConvertToBool(Console.ReadLine());
 
-  
-        Console.WriteLine($"And({firstInput}, {secondInput}) ➞ {And(firstInput, secondInput)}");
-    }
+            static int Points(int twoPointers, int threePointers)
+            {
+                return (twoPointers * 2) + (threePointers * 3);
+            }
 
-    static bool ConvertToBool(string input)
-    {
-        return input.ToLower() == "true";
-    }
+            static void LogicalAndExample()
+            {
+                Console.WriteLine("Testing Logical AND (&&) with true and false:");
 
-    static bool And(bool a, bool b)
-    {
-        return a && b;
-    }
 
-    static void ConcatenateNameWithEdabit()
-    {
-        Console.WriteLine("Please enter a name to concatenate with 'Edabit':");
-        string name = Console.ReadLine();
-        string result = nameString(name);
-        Console.WriteLine($"Result: {result}");
-    }
+                Console.WriteLine("Enter the first boolean value (true/false):");
+                bool firstInput = ConvertToBool(Console.ReadLine());
 
-    static string nameString(string name)
-    {
-        return name + "Edabit";
-    }
+                Console.WriteLine("Enter the second boolean value (true/false):");
+                bool secondInput = ConvertToBool(Console.ReadLine());
 
-    static void GiveMeSomething()
-    {
-        Console.WriteLine("Please provide a string to join with 'something':");
-        string input = Console.ReadLine();
-        Console.WriteLine($"something {input}");
-    }
 
-    static void PerformSum()
-    {
-        Console.WriteLine("Please put two numbers for us to add.");
-        int number1 = int.Parse(Console.ReadLine());
-        int number2 = int.Parse(Console.ReadLine());
-        Console.WriteLine($"The sum of {number1} and {number2} is: {Sum(number1, number2)}");
-    }
+                Console.WriteLine($"And({firstInput}, {secondInput}) ➞ {And(firstInput, secondInput)}");
+            }
 
-    static void ConvertMinutesToSeconds()
-    {
-        Console.WriteLine("Give me a number to convert from minutes to seconds:");
-        int minutes = int.Parse(Console.ReadLine());
-        Console.WriteLine($"{minutes} minutes converted to seconds is: {Convert(minutes)} seconds");
-    }
+            static bool ConvertToBool(string input)
+            {
+                return input.ToLower() == "true";
+            }
 
-    static void AddOne()
-    {
-        Console.WriteLine("Please insert a number to add 1 to:");
-        int number = int.Parse(Console.ReadLine());
-        Console.WriteLine($"{number} plus one is: {PlusOne(number)}");
-    }
+            static bool And(bool a, bool b)
+            {
+                return a && b;
+            }
 
-    static void CalculatePower()
-    {
-        Console.WriteLine("Please enter the voltage:");
-        int voltage = int.Parse(Console.ReadLine());
-        Console.WriteLine("Now insert the current:");
-        int current = int.Parse(Console.ReadLine());
-        Console.WriteLine($"The power of {voltage} and {current} is equal to: {CircuitPower(voltage, current)}");
-    }
+            static void ConcatenateNameWithEdabit()
+            {
+                Console.WriteLine("Please enter a name to concatenate with 'Edabit':");
+                string name = Console.ReadLine();
+                string result = nameString(name);
+                Console.WriteLine($"Result: {result}");
+            }
 
-    static void CalculateAge()
-    {
-        Console.WriteLine("Please enter an age:");
-        int age = int.Parse(Console.ReadLine());
-        Console.WriteLine($"You are {CalcAge(age)} days old.");
-    }
+            static string nameString(string name)
+            {
+                return name + "Edabit";
+            }
 
-    static void CalculateTriangleArea()
-    {
-        Console.WriteLine("Enter the height of the triangle:");
-        float height = float.Parse(Console.ReadLine());
-        Console.WriteLine("Now enter the base of the triangle:");
-        float baseLength = float.Parse(Console.ReadLine());
-        Console.WriteLine($"The area of the triangle is: {TriAge(height, baseLength)}");
-    }
+            static void GiveMeSomething()
+            {
+                Console.WriteLine("Please provide a string to join with 'something':");
+                string input = Console.ReadLine();
+                Console.WriteLine($"something {input}");
+            }
 
-    static void CheckLessThanOrEqualToZero()
-    {
-        Console.WriteLine("Please input a number:");
-        int number = int.Parse(Console.ReadLine());
-        Console.WriteLine($"The number entered is less than or equal to zero: {LessThanOrEqualToZero(number)}");
-    }
+            static void PerformSum()
+            {
+                Console.WriteLine("Please put two numbers for us to add.");
+                int number1 = int.Parse(Console.ReadLine());
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The sum of {number1} and {number2} is: {Sum(number1, number2)}");
+            }
 
-    static void CheckLessThanOneHundred()
-    {
-        Console.WriteLine("Please input the first number:");
-        int number1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Now enter the second number:");
-        int number2 = int.Parse(Console.ReadLine());
-        Console.WriteLine($"The sum of the numbers entered is less than 100: {LessThanOneHundred(number1, number2)}");
-    }
+            static void ConvertMinutesToSeconds()
+            {
+                Console.WriteLine("Give me a number to convert from minutes to seconds:");
+                int minutes = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{minutes} minutes converted to seconds is: {Convert(minutes)} seconds");
+            }
 
-    static void CheckNumbersEqualTo()
-    {
-        Console.WriteLine("Enter the first number:");
-        int number1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Now insert the second number:");
-        int number2 = int.Parse(Console.ReadLine());
-        Console.WriteLine($"The numbers entered are equal: {NumbersEqualTo(number1, number2)}");
-    }
+            static void AddOne()
+            {
+                Console.WriteLine("Please insert a number to add 1 to:");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{number} plus one is: {PlusOne(number)}");
+            }
 
-    static bool Reverse(bool value)
-    {
-        return !value;
-    }
+            static void CalculatePower()
+            {
+                Console.WriteLine("Please enter the voltage:");
+                int voltage = int.Parse(Console.ReadLine());
+                Console.WriteLine("Now insert the current:");
+                int current = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The power of {voltage} and {current} is equal to: {CircuitPower(voltage, current)}");
+            }
 
-    static void ReverseBooleanExample()
-    {
-        Console.WriteLine("Reverse(true) ➞ " + Reverse(true));
-        Console.WriteLine("Reverse(false) ➞ " + Reverse(false));
-    }
+            static void CalculateAge()
+            {
+                Console.WriteLine("Please enter an age:");
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"You are {CalcAge(age)} days old.");
+            }
 
-    static int HowManySeconds(int hours)
-    {
-        return hours * 3600;
-    }
+            static void CalculateTriangleArea()
+            {
+                Console.WriteLine("Enter the height of the triangle:");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine("Now enter the base of the triangle:");
+                float baseLength = float.Parse(Console.ReadLine());
+                Console.WriteLine($"The area of the triangle is: {TriAge(height, baseLength)}");
+            }
 
-    static void ConvertHoursToSeconds()
-    {
-        Console.WriteLine("Please enter the number of hours to convert into seconds:");
-        int hours = int.Parse(Console.ReadLine());
-        int seconds = HowManySeconds(hours);
-        Console.WriteLine($"{hours} hour(s) is equal to {seconds} seconds.");
-    }
+            static void CheckLessThanOrEqualToZero()
+            {
+                Console.WriteLine("Please input a number:");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The number entered is less than or equal to zero: {LessThanOrEqualToZero(number)}");
+            }
 
-    static void SumPolygon()
-    {
-        Console.WriteLine("Enter the number of sides of the polygon (n > 2):");
-        int n = int.Parse(Console.ReadLine());
-        if (n > 2)
-        {
-            int sumOfAngles = (n - 2) * 180;
-            Console.WriteLine($"The sum of internal angles of a {n}-sided polygon is: {sumOfAngles} degrees.");
+            static void CheckLessThanOneHundred()
+            {
+                Console.WriteLine("Please input the first number:");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Now enter the second number:");
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The sum of the numbers entered is less than 100: {LessThanOneHundred(number1, number2)}");
+            }
+
+            static void CheckNumbersEqualTo()
+            {
+                Console.WriteLine("Enter the first number:");
+                int number1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Now insert the second number:");
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The numbers entered are equal: {NumbersEqualTo(number1, number2)}");
+            }
+
+            static bool Reverse(bool value)
+            {
+                return !value;
+            }
+
+            static void ReverseBooleanExample()
+            {
+                Console.WriteLine("Reverse(true) ➞ " + Reverse(true));
+                Console.WriteLine("Reverse(false) ➞ " + Reverse(false));
+            }
+
+            static int HowManySeconds(int hours)
+            {
+                return hours * 3600;
+            }
+
+            static void ConvertHoursToSeconds()
+            {
+                Console.WriteLine("Please enter the number of hours to convert into seconds:");
+                int hours = int.Parse(Console.ReadLine());
+                int seconds = HowManySeconds(hours);
+                Console.WriteLine($"{hours} hour(s) is equal to {seconds} seconds.");
+            }
+
+            static void SumPolygon()
+            {
+                Console.WriteLine("Enter the number of sides of the polygon (n > 2):");
+                int n = int.Parse(Console.ReadLine());
+                if (n > 2)
+                {
+                    int sumOfAngles = (n - 2) * 180;
+                    Console.WriteLine($"The sum of internal angles of a {n}-sided polygon is: {sumOfAngles} degrees.");
+                }
+                else
+                {
+                    Console.WriteLine("The number of sides must be greater than 2.");
+                }
+            }
+
+            static int Sum(int number1, int number2) => number1 + number2;
+            static int Convert(int number) => number * 60;
+            static int PlusOne(int number) => number + 1;
+            static int CircuitPower(int voltage, int current) => voltage * current;
+            static int CalcAge(int years) => years * 365;
+            static float TriAge(float height, float baseLength) => (height * baseLength) / 2;
+            static bool LessThanOrEqualToZero(int number) => number <= 0;
+            static bool LessThanOneHundred(int number1, int number2) => (number1 + number2) < 100;
+            static bool NumbersEqualTo(int number1, int number2) => number1 == number2;
         }
-        else
-        {
-            Console.WriteLine("The number of sides must be greater than 2.");
-        }
-    }
-
-    static int Sum(int number1, int number2) => number1 + number2;
-    static int Convert(int number) => number * 60;
-    static int PlusOne(int number) => number + 1;
-    static int CircuitPower(int voltage, int current) => voltage * current;
-    static int CalcAge(int years) => years * 365;
-    static float TriAge(float height, float baseLength) => (height * baseLength) / 2;
-    static bool LessThanOrEqualToZero(int number) => number <= 0;
-    static bool LessThanOneHundred(int number1, int number2) => (number1 + number2) < 100;
-    static bool NumbersEqualTo(int number1, int number2) => number1 == number2;
 }
